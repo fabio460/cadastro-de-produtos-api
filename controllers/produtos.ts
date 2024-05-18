20f4f14719b4a598d632bc05d03fbc203dbdfdd2
Fabio da Silva de Oliveira <dasilvadeoliveirafabio@gmail.com>
2024-05-18 14:23:20 -0300

    crud produtos

--- controllers/produtos.ts
+++ controllers/produtos.ts
@@ -5,4 +5,56 @@ export const listar = async(req:Request, res:Response)=>{
     const r = await prisma.produto.findMany()
     res.json(r)
 }
+export const listarPorId = async(req:Request, res:Response)=>{
+    const id = req.params.id
+    const r = await prisma.produto.findUnique({where:{id}})
+    res.json(r)
+}
 
+export const criar = async(req:Request, res:Response)=>{
+    const {nome,preco,imagem} = req.body
+    try {
+        const r = await prisma.produto.create({
+           data:{
+              nome,
+              preco,
+              imagem
+           }
+        })
+        res.send("Produto cadastrado com sucesso!")
+        
+    } catch (error) {
+        res.status(500).send(error)
+    }
+}
+
+export const atualizar = async(req:Request, res:Response)=>{
+    const {id,nome,preco,imagem} = req.body
+    try {
+        const r = await prisma.produto.update({
+           data:{
+              nome,
+              preco,
+              imagem
+           },
+           where:{id}
+        })
+        res.send("Produto atualizado com sucesso!")
+        
+    } catch (error) {
+        res.status(500).send(error)
+    }
+}
+
+export const deletar = async(req:Request, res:Response)=>{
+    const {id} = req.body
+    try {
+        const r = await prisma.produto.delete({
+          where:{id}
+        })
+        res.send("Produto deletado com sucesso!")
+        
+    } catch (error) {
+        res.status(500).send(error)
+    }
+}
--- routes/produtoRouter.ts
+++ routes/produtoRouter.ts
@@ -1,5 +1,10 @@
 import { Router } from "express";
-import { listar } from "../controllers/produtos";
+import { atualizar, criar, deletar, listar, listarPorId } from "../controllers/produtos";
 const produtoRouter = Router()
 produtoRouter.get("/listar",listar)
+produtoRouter.get("/listarPorId/:id",listarPorId)
+produtoRouter.post("/cadastrar",criar)
+produtoRouter.put("/atualizar",atualizar)
+produtoRouter.delete("/deletar",deletar)
+
 export default produtoRouter
